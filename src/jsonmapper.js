928b9dd0d6c7a56f01837c06a219bbf22ec87b48
Chen, Hung <Hung.Chen@Pearson.com>
2016-10-18 10:35:56 -0400

    initial commit

---
+++ src/jsonmapper.js
@@ -0,0 +1,49 @@
+const jsonMapper = (obj, config) => {
+	let objString = JSON.stringify(obj);
+	const regex = /\${(.*?)}/g;
+	let matches;
+	let flattenedConfig = flattenObject(config);
+	while((matches = regex.exec(objString)) !== null) {
+		const token = matches[0];
+		const key = matches[1];
+		objString = objString.replace(token, flattenedConfig[key]);
+	}
+
+	return JSON.parse(objString, function(k, v) {
+		if(!isNaN(parseFloat(v))) {
+			return parseFloat(v);
+		}
+		if(v === 'true' || v === 'True') {
+			return true;
+		}
+		if(v === 'false' || v === 'False') {
+			return false;
+		}
+		return v;
+	});
+};
+
+const flattenObject = (ob) => {
+	let toReturn = {};
+	
+	for (let i in ob) {
+		if (!ob.hasOwnProperty(i)) continue;
+		
+		if ((typeof ob[i]) == 'object') {
+			let flatObject = flattenObject(ob[i]);
+			for (let x in flatObject) {
+				if (!flatObject.hasOwnProperty(x)) continue;
+				
+				toReturn[i + '.' + x] = flatObject[x];
+			}
+		} else {
+			toReturn[i] = ob[i];
+		}
+	}
+	return toReturn;
+};
+
+module.exports = {
+  jsonMapper,
+  flattenObject
+};
